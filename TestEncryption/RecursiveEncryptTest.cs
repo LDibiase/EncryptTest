fea5acf9edfe3c19444fd792489402e99858d3e4
LDibiase <lucas.dibiase@gmail.com>
2018-04-30 23:13:45 -0300

    Missing recursive test

---
+++ TestEncryption/RecursiveEncryptTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using EncryptTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestEncryption
+{
+    [TestClass]
+    public class RecursiveEncryptTest
+    {
+        [TestMethod]
+        public void RecursiveEncription_OK()
+        {
+            string actual = "Testing";
+            string expected = "gnitseT";
+            string result = string.Empty;
+
+            RecursiveEncrypt recursiveEncryptor = new RecursiveEncrypt();
+            result = recursiveEncryptor.Encrypt(actual);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
